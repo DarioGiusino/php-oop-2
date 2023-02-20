d437dc646150ee3d6f69962b8f60340791238816
DarioGiusino <dario.giusino@gmail.com>
2023-02-20 15:49:36 +0100

    category to product

--- models/Product.php
+++ models/Product.php
@@ -5,12 +5,14 @@ class Product
     public $image;
     public $label;
     public $description;
+    public $category;
     public $price;
 
-    public function __construct($_label, $_description, $_price, $_image = null)
+    public function __construct($_label, $_description, $_category, $_price, $_image = null)
     {
         $this->label = $_label;
         $this->description = $_description;
+        $this->category = $_category;
         $this->price = $_price;
         $this->image = $_image;
     }
