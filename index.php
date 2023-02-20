30b0154ee55edd90475d8f2ab9830d69ece5a3a8
DarioGiusino <dario.giusino@gmail.com>
2023-02-20 16:16:10 +0100

    added assets

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ var_dump($gioco);
             <div class="row row-cols-3">
                 <div class="col mb-4">
                     <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
+                        <img src="assets/cat_food.png" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
