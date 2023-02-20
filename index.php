f4a0043746e0e556658e5b41abcb015b178e9b7d
DarioGiusino <dario.giusino@gmail.com>
2023-02-20 15:49:23 +0100

    moved category prop to product

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include_once __DIR__ . '/models/Product.php';
 include_once __DIR__ . '/models/Food.php';
 
-$prodotto = new Food('scatola di tonno', 'cibo a base di pesce', 10, 'Cani', '120g', 'pane, acqua');
+$prodotto = new Food('scatola di tonno', 'cibo a base di pesce', 'Cani', 10, '120g', 'pane, acqua');
 
 var_dump($prodotto);
 
--- models/Food.php
+++ models/Food.php
@@ -8,9 +8,9 @@ class Food extends Product
     public $weight;
     public $ingredients;
 
-    public function __construct($_label, $_description, $_price, $_category, $_weight, $_ingredients)
+    public function __construct($_label, $_description, $_category, $_price, $_weight, $_ingredients)
     {
-        parent::__construct($_label, $_description, $_price);
+        parent::__construct($_label, $_description, $_category, $_price);
 
         $this->category = $_category;
         $this->weight = $_weight;
