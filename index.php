88e2435bf77826e30c9453bd6f22b5271f665bed
DarioGiusino <dario.giusino@gmail.com>
2023-02-20 16:12:39 +0100

    defined two category in index

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 include_once __DIR__ . '/models/Product.php';
+include_once __DIR__ . '/models/Category.php';
 include_once __DIR__ . '/models/Food.php';
 include_once __DIR__ . '/models/Kennel.php';
 include_once __DIR__ . '/models/Toy.php';
 
-$cibo = new Food('scatola di tonno', 'cibo a base di pesce', 'Cani', 10, '120g', 'pane, acqua');
-$cuccia = new Kennel('Cuccia Rodi', 'cuccia rettangolare', 'Gatti', 29, '15x17cm', 'red');
-$gioco = new Toy('pallina', 'pallina rotonda', 'Topi', 12, 'plastica e metallo');
+$dog_category = new Category('Cani');
+$cat_category = new Category('Gatti');
+
+$cibo = new Food('scatola di tonno', 'cibo a base di pesce', $dog_category, 10, '120g', 'pane, acqua');
+$cuccia = new Kennel('Cuccia Rodi', 'cuccia rettangolare', $cat_category, 29, '15x17cm', 'red');
+$gioco = new Toy('pallina', 'pallina rotonda', $dog_category, 12, 'plastica e metallo');
 
 var_dump($cibo);
 var_dump($cuccia);
