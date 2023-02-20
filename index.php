027dfc2b6929f98afee1e278fd2b13cdb418297b
DarioGiusino <dario.giusino@gmail.com>
2023-02-20 15:46:01 +0100

    defined a product card example

--- index.php
+++ index.php
@@ -21,6 +21,25 @@ var_dump($prodotto);
 </head>
 
 <body>
+    <header>
+        <h1 class="text-center my-4">Your animal eShop</h1>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row row-cols-3">
+                <div class="col mb-4">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 
 </body>
 
