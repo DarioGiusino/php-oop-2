d43b2f9f1ddee0ca407588048a96cb168dc39309
DarioGiusino <dario.giusino@gmail.com>
2023-02-20 15:13:39 +0100

    product obj

---
+++ models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+class Product
+{
+    public $image;
+    public $label;
+    public $description;
+    public $price;
+
+    public function __construct($_label, $_description, $_price, $_image)
+    {
+        $this->label = $_label;
+        $this->description = $_description;
+        $this->price = $_price;
+        $this->image = $_image;
+    }
+}
